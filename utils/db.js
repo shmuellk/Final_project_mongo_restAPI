d12474f11c3666b94b83f88218677a025168d993
shmuellk <shmuellk32@gmail.com>
2025-06-11 09:55:02 +0300

    delete useNewUrlParser

--- utils/db.js
+++ utils/db.js
@@ -15,8 +15,6 @@ require("dotenv").config();
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
       dbName: "Server",
     });
     if (process.env.NODE_ENV !== "test") {
